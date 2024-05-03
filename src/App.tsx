bc25dd65ffb8e0d77b0c6d599cdcfcd92301ab7f
zuarel <zuev@profi.travel>
2024-05-03 22:14:09 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from './hooks/useTonConnect';
+import { useCounterContract } from './hooks/useCounterContract';
+
+function App() {
+  const { connected } = useTonConnect();
+  const { value, address, sendIncrement } = useCounterContract();
+
+  return (
+    <div className='App'>
+      <div className='Container'>
+        <TonConnectButton style={{margin: 'auto'}}/>
+
+        <div className='Card'>
+          <b>Counter Address</b>
+          <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{value ?? 'Loading...'}</div>
+        </div>
+
+        <button
+          className={`Button ${connected ? 'Active' : 'Disabled'}`}
+          onClick={() => {
+            sendIncrement();
+          }}
+        >
+          Increment
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App
